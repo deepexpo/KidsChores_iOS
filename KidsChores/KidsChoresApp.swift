540f9c24136f3bf9b4fd3a4f8dc7961c194476ee
Kuldeep SIngh <kuldeep.expo@gmail.com>
2026-08-01 09:17:11 -0600

    Initial Commit

---
+++ KidsChores/KidsChoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsChoresApp.swift
+//  KidsChores
+//
+//  Created by Kuldeep Singh on 2026-08-01.
+//
+
+import SwiftUI
+
+@main
+struct KidsChoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
